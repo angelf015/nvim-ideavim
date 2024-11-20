175f22d03c4d4e67b6ba7a996126a7ac544c6745
angelf015 <angelf015.lf@gmail.com>
2024-11-20 00:01:06 -0600

    Refactor ui

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -22,11 +22,6 @@ return {
                     "jdtls",
                     "cssls",
                     "angularls",
-                    "stylua",
-                    "selene",
-                    "luacheck",
-                    "shellcheck",
-                    "shfmt",
                     "tailwindcss",
                 },
             })
